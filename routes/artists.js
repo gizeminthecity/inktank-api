af100b895790ad38881d141a0abfdcfa99236852
Gizem <gizemwanka@gmail.com>
2021-06-05 15:45:16 +0200

    add artists and single artist route

--- routes/artists.js
+++ routes/artists.js
@@ -2,10 +2,23 @@ const router = require("express").Router();
 const Artist = require("../models/Artist.model");
 
 router.get("/", (req, res) => {
-    Artist.find({}).then((allArtists) => {
-        console.log(allArtists);
-        res.json(allArtists);
-    });
+    Artist.find({})
+        .then((allArtists) => {
+            console.log(allArtists);
+            res.json(allArtists);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
 });
 
+router.get("/:artistId", (req, res) => {
+    Artist.findById(req.params.artistId)
+        .then((artist) => {
+            res.json(artist);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+});
 module.exports = router;
